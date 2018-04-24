7d27d26ace077b83839c47d1a312c6d3a87dcff7
n-futagami <n-futagami@cloudpack.jp>
2018-04-24 18:43:26 +0900

    fix error

--- camera/main.py
+++ camera/main.py
@@ -1,51 +1,29 @@
-from flask import Flask, render_template, Response
-
+from __future__ import print_function
+from flask import Flask, Response
 from pyzbar import pyzbar
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 from datetime import datetime
+
 import numpy as np
 import cv2
 import time
 
-from processor.simple_streamer import SimpleStreamer as VideoCamera
-# from processor.pedestrian_detector import PedestrianDetector as VideoCamera
-# from processor.motion_detector import MotionDetector as VideoCamera
-import time
-import threading
-
-video_camera = VideoCamera(flip=False)
 
 camera = PiCamera()
 camera.resolution = (640, 480)
 camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(640, 480))
-time.sleep(0.5)
+time.sleep(0.1)
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(video_camera),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False, threaded=True)
 
 @app.route('/stream')
 def stream():
     return Response(gen(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
 def gen():
     while True:
         frame = get_frame()
@@ -59,5 +37,26 @@ def get_frame():
     frame = display(frame, decoded_objs)
     ret, jpeg = cv2.imencode('.jpg', frame)
     rawCapture.truncate(0)
+
     return jpeg.tobytes()
+
+def decode(frame):
+    decoded_objs = pyzbar.decode(frame, scan_locations=True)
+    for obj in decoded_objs:
+        print(datetime.now().strftime('%H:%M:%S.%f'))
+        print('Type: ', obj.type)
+        print('Data: ', obj.data)
     
+    return decoded_objs
+        
+def display(frame, decoded_objs):
+    for decoded_obj in decoded_objs:
+        left, top, width, height = decoded_obj.rect
+        frame = cv2.rectangle(frame,
+                              (left, top),
+                              (left + width, height + top),
+                              (0, 0, 255), 2)
+    return frame
+    
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=False, threaded=True)
